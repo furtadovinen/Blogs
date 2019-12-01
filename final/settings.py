653ac99ca9da088aa1d7d0a9f133242d610ecf5e
Vinen Furtado <vinenfurtado@vinens-mbp.home>
2019-11-30 23:30:13 -0500

    2nd

--- final/settings.py
+++ final/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'final.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'finaldb',
+        'USER': 'postgres',
+        'PASSWORD': '#Vinen1995',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
